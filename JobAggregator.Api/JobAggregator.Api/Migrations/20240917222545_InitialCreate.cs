fc7f471732e7aba4cc16dfb7e9002f233ed6ca96
Lukáš Bašista <lukasbasista@gmail.com>
2024-10-30 15:45:23 +0100

    ADD: Migrations

---
+++ JobAggregator.Api/JobAggregator.Api/Migrations/20240917222545_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobAggregator.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portals",
+                columns: table => new
+                {
+                    PortalID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PortalName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BaseUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portals", x => x.PortalID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobPostings",
+                columns: table => new
+                {
+                    JobPostingID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ApplyUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PortalID = table.Column<int>(type: "int", nullable: false),
+                    ExternalID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateScraped = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HashCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Salary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobPostings", x => x.JobPostingID);
+                    table.ForeignKey(
+                        name: "FK_JobPostings_Portals_PortalID",
+                        column: x => x.PortalID,
+                        principalTable: "Portals",
+                        principalColumn: "PortalID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_HashCode",
+                table: "JobPostings",
+                column: "HashCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_PortalID",
+                table: "JobPostings",
+                column: "PortalID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobPostings");
+
+            migrationBuilder.DropTable(
+                name: "Portals");
+        }
+    }
+}
---
+++ JobAggregator.Api/JobAggregator.Api/Migrations/20241029181212_AddPortalLogoUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobAggregator.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPortalLogoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PortalLogoUrl",
+                table: "Portals",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PortalLogoUrl",
+                table: "Portals");
+        }
+    }
+}
