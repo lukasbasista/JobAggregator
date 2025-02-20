09b2f6076c6a790340d9cc29176d085bb7891980
Lukáš Bašista <lukasbasista@gmail.com>
2025-02-20 23:13:11 +0100

    Update model changes

---
+++ JobAggregator.Api/JobAggregator.Api/Migrations/20250220221244_UpdateModelChanges.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobAggregator.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_JobPostings_ExternalID",
+                table: "JobPostings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobPostings_HashCode",
+                table: "JobPostings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HashCode",
+                table: "JobPostings",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalID",
+                table: "JobPostings",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_ExternalID",
+                table: "JobPostings",
+                column: "ExternalID",
+                unique: true,
+                filter: "[ExternalID] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_HashCode",
+                table: "JobPostings",
+                column: "HashCode",
+                unique: true,
+                filter: "[HashCode] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_JobPostings_ExternalID",
+                table: "JobPostings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobPostings_HashCode",
+                table: "JobPostings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HashCode",
+                table: "JobPostings",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalID",
+                table: "JobPostings",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_ExternalID",
+                table: "JobPostings",
+                column: "ExternalID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_HashCode",
+                table: "JobPostings",
+                column: "HashCode",
+                unique: true);
+        }
+    }
+}
